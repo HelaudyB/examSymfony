a55f212fc30af10f22a61b630c67732d955207dc
HelaudyB <e.belguet@gmail.com>
2019-05-06 12:01:18 +0200

    first commit

---
+++ src/Controller/ArticleController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Article;
+use App\Form\ArticleType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ArticleController extends AbstractController
+{
+    /**
+     * @Route("/article", name="article")
+     */
+    public function index() :  Response
+    {
+        $repo = $this->getDoctrine()->getRepository(Article::class);
+       // dump($repo);
+        $article = $repo->findAll();
+        return $this->render('article/index.html.twig', [
+        'article' => $article,
+            'controller_name'=>'Articles'
+        ]);
+    }
+    /**
+     * @Route("/article/detail", name="showarticle")
+     */
+    public function showArticle(Article $article)
+    {
+        return $this->render('article/articledetail.html.twig', [
+            'controller_name' => 'les détails de l\'article',
+            'article'=> $article
+        ]);
+    }
+
+    /**
+     * @Route("/article/{article}", name="addarticle", requirements={"article"="^(?!register).+"})
+     */
+    public function addArticle(Request $request)
+    {
+        $form = $this->createForm(ArticleType::class, new Article());
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $article = $form->getData();
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($article);
+            $em->flush();
+        } else {
+            return $this->render('article/addarticle.html.twig', [
+                'form' => $form->createView(),
+                'errors' => $form->getErrors()
+
+            ]);
+        }
+        return $this->render('home/index.html.twig') ;
+    }
+
+}
